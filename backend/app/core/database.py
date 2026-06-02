aff94b27cde10ed992a356109553ae203603e94d
Muhammad Faris Revansyah <farisrevan13@gmail.com>
2026-06-02 23:41:31 +0700

    fix: database connection

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,7 +1,12 @@
 from sqlmodel import SQLModel, create_engine, Session
 from app.core.config import settings
 
-engine = create_engine(settings.DATABASE_URL, echo=True)
+engine = create_engine(
+    settings.DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=3600,
+    connect_args={"sslmode": "require"}
+)
 
 def session():
     with Session(engine) as session:
