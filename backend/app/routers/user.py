fee988c3b7721b0ab6543d1b30234fd404c2fcec
Muhammad Faris Revansyah <farisrevan13@gmail.com>
2026-06-03 20:02:58 +0700

    fix: add missing import

--- backend/app/routers/user.py
+++ backend/app/routers/user.py
@@ -4,16 +4,18 @@ from app.models.user import User
 from app.schemas.user import UserResponse
 from app.routers.scan import get_current_user_id
 from app.core.database import session as get_session
+from httpx import HTTPException
 
 # Create a specialized router for User-related operations
 router = APIRouter(prefix="/users", tags=["Users"])
 
 @router.get("/me", response_model=UserResponse)
 def get_my_profile(
-    user_id: int = Depends(get_current_user_id), # <-- Dikunci pakai JWT!
+    user_id: int = Depends(get_current_user_id),
     db: Session = Depends(get_session)
 ):
     user = db.get(User, user_id)
     if not user:
-        raise HTTPException(status_code=404, detail="User tidak ditemukan")
+        raise HTTPException(status_code=404, detail="User profile not found.")
+    
     return user
